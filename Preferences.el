e83ca42d70fafbf44f0f5490dddd7a3cd0621544
Max Bolingbroke <batterseapower@hotmail.com>
2010-03-24 16:22:52 +0000

    Scion

--- Preferences.el
+++ Preferences.el
@@ -56,3 +56,28 @@
     (and f (string-match "_darcs" f)
          (if (not (y-or-n-p "WARNING: YOU ARE ABOUT TO WRITE TO an _darcs file, are you sure you want to do this? "))
            (keyboard-quit)))))
+
+;; Scion Hooks
+
+;; Substitute the desired version for <version>
+(add-to-list 'load-path "~/.cabal/share/scion-0.1.0.2/emacs")
+(require 'scion)
+
+;; if ./cabal/bin is not in your $PATH
+(setq scion-program "~/.cabal/bin/scion-server")
+
+(defun my-haskell-hook ()
+ (define-key scion-mode-map [f5] 'scion-load)
+ ;; Whenever we open a file in Haskell mode, also activate Scion
+ (scion-mode 1)
+ ;; Whenever a file is saved, immediately type check it and
+ ;; highlight errors/warnings in the source.
+ (scion-flycheck-on-save 1))
+
+(add-hook 'haskell-mode-hook 'my-haskell-hook)
+
+;; Use ido-mode completion (matches anywhere, not just beginning)
+;;
+;; WARNING: This causes some versions of Emacs to fail so badly
+;; that Emacs needs to be restarted.
+(setq scion-completing-read-function 'ido-completing-read)
