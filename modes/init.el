8b63b5959b1e7973f6f1fdb9d6c7dd246e6f3121
Max Bolingbroke <batterseapower@hotmail.com>
2010-03-26 11:28:55 +0000

    Load Textmate mode

--- modes/init.el
+++ modes/init.el
@@ -1,10 +1,17 @@
 ;; ========
+;; Textmate Mode Missing Library Functions
+;; ========
+(defun string-join (separator strings)
+                   (mapconcat 'identity strings separator))
+
+;; ========
 ;; Load Modes
 ;; ========
 (add-subdirectories-of-to-load-path "~/Library/Preferences/Aquamacs Emacs/modes")
 (require 'gitsum)
 (require 'darcsum)
 (require 'haskell-mode)
+(require 'textmate)
 (load-file (let ((coding-system-for-read 'utf-8))
            (shell-command-to-string "agda-mode locate")))
 
